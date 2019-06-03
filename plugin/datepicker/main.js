b435642afcb9d7af586341d2994a9ab9d94e8435
Chong <monkey.d.chong@gmail.com>
2019-06-03 11:50:49 +0700

    s

--- plugin/datepicker/main.js
+++ plugin/datepicker/main.js
@@ -1,28 +1,42 @@
-(function ($) {
+// Utility
+if ( typeof Object.create !== 'function' ) {
+    Object.create = function( obj ) {
+        function F() {};
+        F.prototype = obj;
+        return new F();
+    };
+}
 
+(function( $, window, document, undefined ) {
 
     var DatePicker = {
         init: function (options, elem) { 
             var self = this;
 
+            self.opts = $.extend(true, {}, $.fn.datepicker.defaults, options);
+            console.log( 'DatePicker...', self.opts );
 
-        }
+        },
+
+
+        
     };
 
-    jQuery.fn.datepicker = function (settings) {
-        var config = {'foo': 'bar'};
- 
-        if (settings) jQuery.extend(config, settings);
- 
-        this.each(function () {
-            // element-specific code here
-            
-            
-            console.log( 'datepicker...' );
+
+    // What does the datepicker plugin do?
+    $.fn.datepicker = function(options) {
+        return this.each(function() {
+            var $this = Object.create( DatePicker );
+            $this.init( options, this );
+            $.data( this, 'datepicker', $this );
         });
- 
-        return this;
     };
 
+    // default options
+    $.fn.datepicker.defaults = {
+      defaultOne: true,
+      defaultTwo: false,
+      defaultThree: 'yay!'
+    };
 
-})(jQuery);
+})(jQuery, window, document );
